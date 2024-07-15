d9f9d0451be5afc9ee9c053cf2433d0dbda493da
Gregory Go <gregorygocc@gmail.com>
2024-07-15 17:54:56 +0200

    feat: database reading

--- context-match/main.py
+++ context-match/main.py
@@ -1,5 +1,7 @@
 import json
-from utils import *
+from utils.filter_names import *
+from utils.fetch_table_notion import *
+from notion_client import Client
 from argparse import ArgumentParser
 
 # Argparser arguments:
@@ -8,8 +10,31 @@ from argparse import ArgumentParser
 # Execution
 if __name__ == "__main__":
 
-    # Notion API Token Get
+    # Initialize the Notion client
     with open("notion.json") as f:
         NOTION_TOKEN = json.load(f)["KEY"]
 
-    print(NOTION_TOKEN)
+    notion = Client(auth=NOTION_TOKEN)
+
+    # Replace with your Notion database ID
+    database_id = "6ead038babe946b99854dba84ecf05a9"
+
+    # Fetch the page links from the database
+    page_names, page_links = get_page_links(notion, database_id)
+
+    # Fetch the table links from each page
+    page_table_links = get_table_links_from_pages(notion, page_links)
+
+    # Print the links
+    df_dict = {}
+    for idx, tables in enumerate(page_table_links.values()):
+
+        for table in tables:
+
+            id = table.split("#")
+            temp = f"https://www.notion.so/about-/{id[-1]}?v=eceee883ed684a75831aec55806e39d2"
+            df = get_table_notion(NOTION_TOKEN, temp)
+
+            df_dict[page_names[idx]] = df
+
+    print(df_dict)
--- context-match/utils/fetch_table_notion.py
+++ context-match/utils/fetch_table_notion.py
@@ -1,6 +1,4 @@
-import json
 import notion_df
-from notion_client import Client
 
 
 def get_table_notion(NOTION_TOKEN, DATABASE_URL):
@@ -8,7 +6,7 @@ def get_table_notion(NOTION_TOKEN, DATABASE_URL):
     return notion_df.download(DATABASE_URL, api_key=NOTION_TOKEN)
 
 
-def get_page_links(database_id):
+def get_page_links(notion, database_id):
     """
     Get links to pages in a Notion database.
 
@@ -18,18 +16,21 @@ def get_page_links(database_id):
     response = notion.databases.query(database_id=database_id)
     pages = response.get("results")
 
+    names = []
     links = []
     for page in pages:
 
+        page_title = page["properties"]["Name"]["title"][0]["text"]["content"]
         page_id = page["id"]
         # Extract the page URL
         page_url = f"https://www.notion.so/{page_id.replace('-', '')}"
+        names.append(page_title)
         links.append(page_url)
 
-    return links
+    return names, links
 
 
-def get_table_links_from_pages(page_links):
+def get_table_links_from_pages(notion, page_links):
     """
     Get links to tables in each Notion page.
 
@@ -51,32 +52,3 @@ def get_table_links_from_pages(page_links):
         page_table_links[page_link] = table_links
 
     return page_table_links
-
-
-if __name__ == "__main__":
-
-    # Initialize the Notion client
-    with open("notion.json") as f:
-        NOTION_TOKEN = json.load(f)["KEY"]
-
-    notion = Client(auth=NOTION_TOKEN)
-
-    # Replace with your Notion database ID
-    database_id = "6ead038babe946b99854dba84ecf05a9"
-
-    # Fetch the page links from the database
-    page_links = get_page_links(database_id)
-
-    # Fetch the table links from each page
-    page_table_links = get_table_links_from_pages(page_links)
-
-    # Print the links
-    for page, tables in page_table_links.items():
-
-        for table in tables:
-
-            id = table.split("#")
-            temp = f"https://www.notion.so/about-/{id[-1]}?v=eceee883ed684a75831aec55806e39d2"
-            df = get_table_notion(NOTION_TOKEN, temp)
-
-            print(list(df.columns)[-1])
