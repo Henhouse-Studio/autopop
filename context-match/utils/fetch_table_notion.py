e1247c32fdc46d5bb1bf80b58c66d115babfde75
danilotpnta <danilotpnta@gmail.com>
2024-07-15 15:07:36 +0200

    feat: rename function to get_table_notion

--- context-match/utils/fetch_table_notion.py
+++ context-match/utils/fetch_table_notion.py
@@ -1,4 +1,4 @@
 import notion_df
 
-def get_table(NOTION_TOKEN, DATABASE_URL):
-    return notion_df.download(DATABASE_URL, api_key=NOTION_TOKEN)
+def get_table_notion(NOTION_TOKEN, DATABASE_URL):
+    return noti_notionon_df.download(DATABASE_URL, api_key=NOTION_TOKEN)
