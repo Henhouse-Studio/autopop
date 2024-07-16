fc3dc32152920aab7bed9c3597321658601bc41f
danilotpnta <danilotpnta@gmail.com>
2024-07-16 15:02:56 +0200

    update

--- context-match/main.py
+++ context-match/main.py
@@ -5,18 +5,25 @@ from utils.make_embeddings import *
 from utils.compute_similarity import *
 from notion_client import Client
 from argparse import ArgumentParser
+import time
+import numpy as np
 
 # Argparser arguments:
 
 
 # Execution
 if __name__ == "__main__":
+
+    start_time = time.time()
     
     # Initialize the Notion client
     with open("notion.json") as f:
         NOTION_TOKEN = json.load(f)["KEY"]
 
     notion = Client(auth=NOTION_TOKEN)
+
+    print(f"Initialization time: {time.time() - start_time:.2f} seconds")
+    init_time = time.time()
 
     # The Notion database ID
     database_id = "6ead038babe946b99854dba84ecf05a9"
@@ -25,10 +32,21 @@ if __name__ == "__main__":
     page_names, page_links = get_page_links(notion, database_id)
     page_table_links = get_table_links_from_pages(notion, page_links)
 
+    print(f"Fetching page and table links time: {time.time() - init_time:.2f} seconds")
+    fetch_links_time = time.time()
+
     # Prompt user inputs:
     prompt = "Get me a table of people, where they work and their blogs"
     prompt_embedding = compute_embedding(prompt)
-    print(prompt_embedding.size())
+    print(prompt_embedding.shape)
+
+    # Save the prompt embedding in npy file
+    np.save("prompt_embedding.npy", prompt_embedding)
+    
+    # load prompt embedding if already saved
+    prompt_embedding = np.load("prompt_embedding.npy")    
+
+    print(f"Computing prompt embedding time: {time.time() - fetch_links_time:.2f} seconds")
 
     # Converting the databases to pandas dataframes
     df_dict = {}
@@ -48,7 +66,7 @@ if __name__ == "__main__":
             field_embeddings = compute_embedding(context)
             # similarity_score = compute_similarity(prompt_embedding, field_embeddings)
 
-            print(field_embeddings.size())
+            print(field_embeddings.shape)
 
             # df_dict[page_names[idx]] = (similarity_score, df)
 
