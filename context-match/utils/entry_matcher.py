79688bae5a59543daef9e6be2ba58fbfb82f06a7
danilotpnta <danilotpnta@gmail.com>
2024-07-24 16:24:44 +0200

    feat: include rows from base that do not have a match w/ df_pop

--- context-match/utils/entry_matcher.py
+++ context-match/utils/entry_matcher.py
@@ -9,16 +9,34 @@ def combine_dfs(
 
     # Iterate over the pointers to merge the rows
     pointers = list(filtered_similarity_scores.keys())
+
+    # Get the first element of pointers into a list 
+    pointers_base = [pointer[0] for pointer in pointers]
+    remaining_pointers = [(index, "empty") for index in range(len(df_base)) if index not in pointers_base]
+    pointers += remaining_pointers
+
+    # sort the pointers by descending order by the first element of the tuple
+    pointers = sorted(pointers, key=lambda x: x[0])
+    
+    # create an emtpy dataframe with col size as df_pop_col_size
+    df_pop_col_size = len(df_populate.columns)
+    empty_row = pd.Series([None] * df_pop_col_size, index=[f"{col}_df2" for col in df_populate.columns])    
+    
     for i, j in pointers:
 
-        # Get the row from df_base
+         # Get the row from df_base
         df_base_row = df_base.iloc[i].copy()
-        # Get the row from df_populate and rename its columns
-        df_populate_row = df_populate.iloc[j].copy()
-        df_populate_row.index = [f"{col}_df2" for col in df_populate_row.index]
+
+        if j != 'empty':
+            # Get the row from df_populate and rename its columns
+            df_populate_row = df_populate.iloc[j].copy()
+            df_populate_row.index = [f"{col}_df2" for col in df_populate_row.index]
+            merged_row = pd.concat([df_base_row, df_populate_row])
+
+        else:
+            merged_row = pd.concat([df_base_row, empty_row])
 
         # Concatenate the rows along the columns
-        merged_row = pd.concat([df_base_row, df_populate_row])
         merged_data.append(merged_row)
 
     # Convert the list of merged rows to a DataFrame
