6dc31f7691be314d483115950f6f10354ba0c4dd
danilotpnta <danilotpnta@gmail.com>
2024-08-08 17:55:46 +0200

    add handle_prompt to calculate syn and enrich in one line

--- context-match/utils/prompt_expansion.py
+++ context-match/utils/prompt_expansion.py
@@ -54,6 +54,28 @@ def get_synonyms(word, max_synonyms=3):
     return synonyms
 
 
+def handle_prompt(prompt, api_key: str, expand_with_syn: bool, expand_with_openAI: bool, print_prompt: bool):
+    """ 
+    Function to handle the prompt enrichment process
+    
+    :param prompt: The prompt to enrich (str).
+    :param api_key: The OpenAI API key (str).
+    :param expand_with_syn: Expand the prompt with synonyms (bool).
+    :param expand_with_openAI: Expand the prompt with OpenAI (bool).
+    :param print_prompt: Print the prompt (bool).
+    :return: The enriched prompt (str).
+    """
+    if expand_with_syn:
+        prompt = expand_prompt_with_synonyms(prompt)
+
+    if expand_with_openAI:
+        prompt = get_enriched_prompt(prompt, api_key=api_key)
+
+    if print_prompt:
+         print(f"The input prompt is: \n{prompt}")
+    
+    return prompt
+
 # Function to expand prompt with synonyms and append as keywords
 def expand_prompt_with_synonyms(prompt, max_synonyms_per_word=2):
     stop_words = set(stopwords.words("english"))
@@ -74,7 +96,7 @@ def expand_prompt_with_synonyms(prompt, max_synonyms_per_word=2):
                 keywords.add(synonym)
                 added_words.add(synonym)
 
-    keywords_str = "\nkeywords: " + ", ".join(keywords)
+    keywords_str = "\n- These are keywords extracted from the prompt: " + ", ".join(keywords)
     return prompt + ", " + keywords_str
 
 def get_enriched_prompt(original_prompt: str, api_key: str):
@@ -91,10 +113,20 @@ def get_enriched_prompt(original_prompt: str, api_key: str):
     response = sub("```python", "", response)
     response = sub("```", "", response)
 
-    # make response into a string
-    # response = json.loads(response)
-    # response = ', '.join(response)
+      # Try to decode the JSON
+    try:
+        response_json = json.loads(response)
+    except json.JSONDecodeError as e:
+        print(f"JSON decode error: {e}")
+        raise
 
-    enriched_prompt = original_prompt + ". The table should contain column names similar to: " + response
+    # Ensure response_json is a list or dict
+    if not isinstance(response_json, (list, dict)):
+        raise ValueError("Decoded JSON is not a list or dict")
+
+    # Convert the JSON response to a string
+    response_str = ', '.join(response_json) if isinstance(response_json, list) else ', '.join(response_json.values())
+
+    enriched_prompt = original_prompt + ".\n- The table should contain column names similar to: " + response_str + '\n'
 
     return enriched_prompt
